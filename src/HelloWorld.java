71e8fe8c6a791a283f86e44b3aa4fe58bec8f3ec
mkislyuk <93876843+mkislyuk@users.noreply.github.com>
2022-01-10 11:45:17 -0800

    work3 is done ana ready to pushed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,4 +5,8 @@ public class HelloWorld {
 
     //Doing awesome work 2!!
 
+    //Doing awesome work 3!
+    //Awesome work 3 is done and ready to committed
+    //and ready to be pushed to remote repository
+
 }
